66a1fa42a9e42fc3c19ae318250509320612eb5f
ktrancoso <kevin.trancoso@b4star.com>
2021-06-21 18:13:35 +0200

    android things

--- mobile/lib/env/env.dart
+++ mobile/lib/env/env.dart
@@ -2,7 +2,7 @@ const String stage = String.fromEnvironment('APP_ENV', defaultValue: 'dev');
 
 const dev = {
   'api_authority': 'http',
-  'api_endpoint': '127.0.0.1',
+  'api_endpoint': '10.0.2.2',
   'api_port': 3000,
   'api_path': '',
 };
